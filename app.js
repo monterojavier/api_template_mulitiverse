54f64c946bddb840593aaf9d08b3cde8699719a1
monterojavier <monterodotjavier@gmail.com>
2022-04-13 11:13:18 -0400

    My first commit

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 //require basicAuth
-const basicAuth = require('express-basic-auth');
+const basicAuth = require("express-basic-auth");
 //require bcrypt
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 // set salt
 const saltRounds = 2;
 
-const {User, Item} = require('./models');
-const { use } = require("bcrypt/promises");
+const {User, Item} = require("./models");
+const {use} = require("bcrypt/promises");
 
 // initialise Express
 const app = express();
@@ -15,10 +15,12 @@ const app = express();
 // specify out request bodies are json
 app.use(express.json());
 
-// routes go here
-app.get('/', (req, res) => {
-  res.send('<h1>App Running</h1>')
-})
+// ROUTES GO HERE
+
+// GET
+app.get("/", (req, res) => {
+  res.send("<h1>App Running</h1>");
+});
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
